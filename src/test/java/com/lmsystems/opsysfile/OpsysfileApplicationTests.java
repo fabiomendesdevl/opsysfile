55a420da826c56b7e915a547c538fc138efa1082
“fabiomendesdevl” <fabiomendesdev@outlook.com>
2025-04-27 20:45:17 -0300

    project create

---
+++ src/test/java/com/lmsystems/opsysfile/OpsysfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmsystems.opsysfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsysfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
